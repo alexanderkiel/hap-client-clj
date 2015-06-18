8b62f7de6b8b6f53c39f5c0cf588f80b67c7e156
Alexander Kiel <alexanderkiel@gmx.net>
2015-06-18 15:49:08 +0200

    Set Group to org.clojars.akiel

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hap-client-clj "0.1-SNAPSHOT"
+(defproject org.clojars.akiel/hap-client-clj "0.1-SNAPSHOT"
   :description "A Clojure HAP client library."
   :url "https://github.com/alexanderkiel/hap-client-clj"
 
