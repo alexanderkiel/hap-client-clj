d40599e351fad9e05c053b4b0ac5d5c0ecdf42b2
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-10 18:21:58 +0200

    Update clojurescript to 1.7.48

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "0.0-3211"]
+                 [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.logging "0.3.1"]
                  [prismatic/plumbing "0.4.3"]
