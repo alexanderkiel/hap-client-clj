bc8c4e8632cc5e5d1025b126bdad31428cfa4b63
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-10 18:23:11 +0200

    Update transit-clj to 0.8.275

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [prismatic/plumbing "0.4.3"]
                  [http-kit "2.1.18"]
-                 [com.cognitect/transit-clj "0.8.275"]
+                 [com.cognitect/transit-clj "0.8.281"]
                  [com.cognitect/transit-cljs "0.8.220"]
                  [org.clojars.akiel/transit-schema "0.1"]]
 
