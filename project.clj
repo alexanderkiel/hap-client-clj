3023d12644b769d3153c91d175f168be680a846d
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-10 18:23:32 +0200

    Update transit-schema to 0.2

--- project.clj
+++ project.clj
@@ -13,6 +13,6 @@
                  [http-kit "2.1.18"]
                  [com.cognitect/transit-clj "0.8.281"]
                  [com.cognitect/transit-cljs "0.8.220"]
-                 [org.clojars.akiel/transit-schema "0.1"]]
+                 [org.clojars.akiel/transit-schema "0.2"]]
 
   :profiles {:dev {:dependencies [[http-kit.fake "0.2.1"]]}})
