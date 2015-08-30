1929a4abd71bc120fd47cf9f0d45f4de166651d5
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-30 20:11:42 +0200

    Update transit-cljs to 0.8.225

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [prismatic/plumbing "0.4.3"]
                  [http-kit "2.1.18"]
                  [com.cognitect/transit-clj "0.8.281"]
-                 [com.cognitect/transit-cljs "0.8.220"]
+                 [com.cognitect/transit-cljs "0.8.225"]
                  [org.clojars.akiel/transit-schema "0.2"]]
 
   :profiles {:dev {:dependencies [[http-kit.fake "0.2.1"]]}})
