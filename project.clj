ff7b0cee1fa898178e63ccb747157143b45637dd
Alexander Kiel <alexanderkiel@gmx.net>
2015-10-30 13:58:15 +0100

    Update Dependencies

--- project.clj
+++ project.clj
@@ -6,12 +6,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/clojurescript "1.7.145"]
+                 [org.clojure/core.async "0.2.371"]
                  [org.clojure/tools.logging "0.3.1"]
                  [prismatic/plumbing "0.4.3"]
                  [http-kit "2.1.18"]
-                 [com.cognitect/transit-clj "0.8.281"]
+                 [com.cognitect/transit-clj "0.8.285"]
                  [com.cognitect/transit-cljs "0.8.225"]
                  [org.clojars.akiel/transit-schema "0.2"]]
 
