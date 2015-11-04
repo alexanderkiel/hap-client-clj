c581360e610f5d7744ceba0295b0ae790c2c2077
Alexander Kiel <alexanderkiel@gmx.net>
2015-11-04 09:28:39 +0100

    Bump Next Dev Version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/hap-client-clj "0.2"
+(defproject org.clojars.akiel/hap-client-clj "0.3-SNAPSHOT"
   :description "A Clojure(Script) HAP client library."
   :url "https://github.com/alexanderkiel/hap-client-clj"
 
