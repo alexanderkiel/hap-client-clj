b8b9deaaea0f7d8095258caf4e7f77063d380610
Alexander Kiel <alexanderkiel@gmx.net>
2015-11-04 09:24:57 +0100

    Update Prismatic Plumbing

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                  [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.2.371"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [prismatic/plumbing "0.4.3"]
+                 [prismatic/plumbing "0.4.4"]
                  [http-kit "2.1.18"]
                  [com.cognitect/transit-clj "0.8.285"]
                  [com.cognitect/transit-cljs "0.8.225"]
