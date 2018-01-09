2cced7a4396f208073c079d5b3970dbe40c7911e
Alexander Kiel <alexanderkiel@gmx.net>
2018-01-09 12:35:33 +0100

    Update Dependencies esp. Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [org.clojure/core.async "0.2.371"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/core.async "0.4.474"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [prismatic/schema "1.0.4"]
+                 [prismatic/schema "1.1.7"]
                  [http-kit "2.1.18"]
-                 [com.cognitect/transit-clj "0.8.285"]
-                 [com.cognitect/transit-cljs "0.8.237"]
+                 [com.cognitect/transit-clj "0.8.300"]
+                 [com.cognitect/transit-cljs "0.8.243"]
                  [org.clojars.akiel/transit-schema "0.4"]]
 
   :profiles {:dev {:dependencies [[http-kit.fake "0.2.1"]]}})
