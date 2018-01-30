c66483f84475052109042e220733d5fa472baf37
Alexander Kiel <alexanderkiel@gmx.net>
2018-01-30 20:51:09 +0100

    Move ClojureScript Dependency into the dev Profile
    I don't like to have the ClojureScript dependency in Clojure projects.

--- project.clj
+++ project.clj
@@ -5,8 +5,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
+  :min-lein-version "2.5.2"
+  :pedantic? :abort
+
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
                  [org.clojure/core.async "0.4.474"]
                  [org.clojure/tools.logging "0.3.1"]
                  [prismatic/schema "1.1.7"]
@@ -15,4 +17,7 @@
                  [com.cognitect/transit-cljs "0.8.243"]
                  [org.clojars.akiel/transit-schema "0.4"]]
 
-  :profiles {:dev {:dependencies [[http-kit.fake "0.2.1"]]}})
+  :profiles {:dev
+             {:dependencies
+              [[org.clojure/clojurescript "1.9.946"]
+               [http-kit.fake "0.2.1"]]}})
